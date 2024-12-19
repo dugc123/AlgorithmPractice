27f21e3256709738a66cc18b47cdb3efed2eb2e2
dugengchen <dugengchen6@163.com>
2024-12-19 15:33:43 +0800

    相交链表

---
+++ 19.相交链表.js
@@ -0,0 +1,34 @@
+class ListNode {
+    constructor(val, next) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+}
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+    if (!headA || !headB) return null;
+
+    var pA = headA;
+    var pB = headB;
+
+    while(pA !== pB) {
+        pA = pA ? pA.next : headB;
+        pB = pB ? pB.next : headA;
+    }
+    return pA;
+};
+
+// 创建相交部分
+var intersectNode = new ListNode(8, new ListNode(4, new ListNode(5)));
+
+// 创建链表 A: 4 -> 1 -> 8 -> 4 -> 5
+var headA = new ListNode(4, new ListNode(1, intersectNode));
+
+// 创建链表 B: 5 -> 6 -> 1 -> 8 -> 4 -> 5
+var headB = new ListNode(5, new ListNode(6, new ListNode(1, intersectNode)));
+console.log('getIntersectionNode', getIntersectionNode(headA, headB));
